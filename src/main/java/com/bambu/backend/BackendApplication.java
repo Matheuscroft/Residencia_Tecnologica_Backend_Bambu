c8ce8894254744a8ed2bec1e79c0ded64a61b431
Matheus OS <matheus.sdoliveira@gmail.com>
2024-11-29 18:05:44 -0300

    documentacao swagger

--- src/main/java/com/bambu/backend/BackendApplication.java
+++ src/main/java/com/bambu/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package com.bambu.backend;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@OpenAPIDefinition(info = @Info(title = "Product API", version="1", description="Api for project management"))
 public class BackendApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/bambu/backend/controllers/ArquitetoController.java
+++ src/main/java/com/bambu/backend/controllers/ArquitetoController.java
@@ -4,6 +4,10 @@ import com.bambu.backend.dtos.ArquitetoDto;
 import com.bambu.backend.models.ArquitetoModel;
 import com.bambu.backend.repositories.ArquitetoRepository;
 import com.bambu.backend.services.ArquitetoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -18,6 +22,7 @@ import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/arquitetos")
+@Tag(name = "Arquitetos", description = "Endpoints para gerenciamento de arquitetos")
 public class ArquitetoController {
 
     private static final Logger logger = LoggerFactory.getLogger(ArquitetoController.class);
@@ -26,6 +31,11 @@ public class ArquitetoController {
     private ArquitetoService arquitetoService;
 
     @PostMapping
+    @Operation(summary = "Criar um novo arquiteto", description = "Adiciona um novo arquiteto ao banco de dados")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Arquiteto criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos")
+    })
     public ResponseEntity<ArquitetoModel> criarArquiteto(@RequestBody ArquitetoDto arquitetoDto) {
 
         logger.info("Recebida solicitação para criar arquiteto: {}", arquitetoDto);
@@ -38,6 +48,11 @@ public class ArquitetoController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Obter um arquiteto pelo ID", description = "Busca os detalhes de um arquiteto pelo ID fornecido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Arquiteto encontrado"),
+            @ApiResponse(responseCode = "404", description = "Arquiteto não encontrado")
+    })
     public ResponseEntity<Object> getArquitetoById(@PathVariable UUID id) {
 
         logger.info("Recebida solicitação para buscar arquiteto com ID: {}", id);
@@ -55,6 +70,10 @@ public class ArquitetoController {
     }
 
     @GetMapping
+    @Operation(summary = "Listar todos os arquitetos", description = "Retorna uma lista de todos os arquitetos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de arquitetos retornada com sucesso")
+    })
     public ResponseEntity<List<ArquitetoModel>> getAllArquitetos(){
         logger.info("Recebida solicitação para listar todos os arquitetos.");
 
@@ -75,6 +94,11 @@ public class ArquitetoController {
 
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Excluir um arquiteto pelo ID", description = "Remove um arquiteto com base no ID fornecido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Arquiteto deletado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Arquiteto não encontrado")
+    })
     public ResponseEntity<String> deleteArquitetoById(@PathVariable UUID id){
         logger.info("Recebida solicitação para deletar arquiteto com ID: {}", id);
 
@@ -87,6 +111,12 @@ public class ArquitetoController {
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar um arquiteto", description = "Atualiza as informações de um arquiteto pelo ID fornecido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Arquiteto atualizado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Arquiteto não encontrado"),
+            @ApiResponse(responseCode = "400", description = "Dados inválidos fornecidos")
+    })
     public ResponseEntity<Object> updateArquitetoById(@RequestBody ArquitetoDto arquitetoDto,
                                                     @PathVariable UUID id){
 
